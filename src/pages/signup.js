5e6795f10a52f6de44d4632aba9760090732bf95
TomaszLaski <tomasz.laski0001@gmail.com>
2021-03-02 12:16:28 +0100

    add SignUp page title, state and validation

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -3,6 +3,18 @@ import { Link } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
 
 export default function SignUp() {
+    const [username, setUsername] = useState('');
+    const [fullName, setFullName] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const [error, setError] = useState('');
+    const isInvalid = username === '' || fullName === '' || password === '' || emailAddress === '';
+    
+    useEffect(() => {
+        document.title = 'Sign Up - Instagram';
+    }, []);
+    
     return (
         <div className="container flex mx-auto max-w-xs items-center h-screen">
             <div className="flex flex-col">
@@ -17,28 +29,38 @@ export default function SignUp() {
                             className="text-sm text-gray w-full mr-3 py-5 px-4 h-2 border bg-gray-background rounded mb-2"
                             type="text"
                             placeholder="Username"
+                            value={username}
+                            onChange={({ target }) => setUsername(target.value.toLowerCase())}
                         />
                         <input
                             aria-label="Enter your full name"
                             className="text-sm text-gray w-full mr-3 py-5 px-4 h-2 border bg-gray-background rounded mb-2"
                             type="text"
                             placeholder="Full name"
+                            value={fullName}
+                            onChange={({ target }) => setFullName(target.value)}
                         />
                         <input
                             aria-label="Enter your email address"
                             className="text-sm text-gray w-full mr-3 py-5 px-4 h-2 border bg-gray-background rounded mb-2"
                             type="text"
                             placeholder="Email address"
+                            value={emailAddress}
+                            onChange={({ target }) => setEmailAddress(target.value.toLowerCase())}
                         />
                         <input
                             aria-label="Enter your password"
                             className="text-sm text-gray w-full mr-3 py-5 px-4 h-2 border bg-gray-background rounded mb-2"
                             type="password"
                             placeholder="Password"
+                            value={password}
+                            onChange={({ target }) => setPassword(target.value)}
                         />
                         <button
+                            disabled={isInvalid}
                             type="submit"
-                            className={`bg-blue-500 text-white w-full rounded h-8 font-bold`}
+                            className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${ isInvalid && 'cursor-not-allowed opacity-50'
+                            }`}
                         >
                             Sign Up
                         </button>
