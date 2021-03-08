362cf22045003cc7a024617ef5372247e463e94b
TomaszLaski <tomasz.laski0001@gmail.com>
2021-03-08 18:59:15 +0100

    create dashboard page

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Header from '../components/header.js';
+import Timeline from '../components/timeline.js';
+import Sidebar from '../components/sidebar/index.js';
 
-export default function Dashboard(){
-    return(
-        <p>hello from Dashboard</p>
+export default function Dashboard() {
+    useEffect(() => {
+        document.title = 'Instagram';
+    }, []);
+       
+    return (
+        <div className="bg-gray-100">
+            <Header />
+            <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+                <Timeline />
+                <Sidebar />
+            </div>
+        </div>
     )
 }
