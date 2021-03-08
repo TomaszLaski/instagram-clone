c406932bef4203b9d8a72127004405dadb06899f
TomaszLaski <tomasz.laski0001@gmail.com>
2021-03-08 20:18:00 +0100

    add svgs for header component

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,9 @@ import * as ROUTES from '../constants/routes';
 
 export default function Header() {
     const { firebase } = useContext(FirebaseContext);
-    const user = null;
+    const user = {
+        displayName: "Tomas"
+    };
     
     return (
         <header className="h-16 bg-white border-b mb-8">
@@ -22,7 +24,20 @@ export default function Header() {
                         {user ? (
                             <>
                                 <Link to={ROUTES.DASHBOARD} arial-label="Home">
-                                    <p>Dashboard</p>
+                                    <svg
+                                        className="w-8 mr-6 text-black-light cursor-pointer"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth={2}
+                                            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                                        />
+                                    </svg>
                                 </Link>
                                 
                                 <button
@@ -35,7 +50,20 @@ export default function Header() {
                                         }
                                     }}
                                 >
-                                    Sign Out
+                                    <svg
+                                        className="w-8 mr-6 text-black-light cursor-pointer"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth={2}
+                                            d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                                        />
+                                    </svg>
                                 </button>
                                 <div className="flex items-center cursor-pointer">
                                     <Link to={`/p/${user.displayName}`}>
