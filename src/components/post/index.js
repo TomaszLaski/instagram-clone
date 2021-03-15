1e7d26e851be1115a9f325315e861427b77e7abc
TomaszLaski <tomasz.laski0001@gmail.com>
2021-03-15 20:30:49 +0100

    Creating Post Actions Component

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import Header from './header';
 import Image from './image';
@@ -7,9 +7,18 @@ import Footer from './footer';
 import Comments from './comments';
 
 export default function Post({ content }) {
+    const commentInput = useRef(null);
+    const handleFocus = () => commentInput.current.focus();
+    
     return (
         <div className="rounded col-span-4 border bg-white mb-16">
             <Image src={content.imageSrc} caption={content.caption} />
+            <Actions
+                docId={content.docId}
+                totalLikes={content.likes.length}
+                likedPhoto={content.userLikedPhoto}
+                handleFocus={handleFocus}
+            />
             <Footer username={content.username} caption={content.caption} />
         </div>
     )
