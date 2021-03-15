194f652a4286fcaf60deec82a0c8a0b836a200f6
TomaszLaski <tomasz.laski0001@gmail.com>
2021-03-15 19:16:26 +0100

    creating Post Component

---
+++ src/components/post/index.js
@@ -0,0 +1,11 @@
+import Header from './header';
+import Image from './image';
+import Actions from './actions';
+import Footer from './footer';
+import Comments from './comments';
+
+export default function Post({ content }) {
+    return (
+        <p>I am a post</p>
+    )
+}
