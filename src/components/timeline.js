efe37ca48d74292631feeb66d1bd856923625297
TomaszLaski <tomasz.laski0001@gmail.com>
2021-03-15 18:32:32 +0100

    add Timeline component

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+
 
 export default function Timeline() {
-    return (<p>I am the timeline</p>);
+    const photos = [1,2,3,4,5];
+    
+    return (
+        <div className="container col-span-2">
+            {!photos ? (
+                <Skeleton count={4} width={640} height={500} className="mb-5" />
+            ) : (
+                photos.map((content) => <p>I will be a photo!</p>)
+            )}
+        </div>
+    );
 }
