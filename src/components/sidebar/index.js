60da71b14ae5c472c08c84fd9015a1c87fc8be75
TomaszLaski <tomasz.laski0001@gmail.com>
2021-03-16 10:31:23 +0100

    Sidebar recap & adding a User component

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import useUser from '../../hooks/use-user';
+import User from './user';
 
 export default function Sidebar() {
-    const { user: { docId, userId, following, username, fullName } = {} } = useUser();
-    return (<p>I am the sidebar</p>);
+    const { 
+        user: { fullName, username, userId } 
+    } = useUser();
+    
+    return (
+        <div className="p-4">
+            <User username={username} fullName={fullName} />
+        </div>
+    );
 }
---
+++ src/components/sidebar/user.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Skelton from 'react-loading-skeleton';
+
+const User = ({ username, fullName }) => 
+    !username || !fullName ? (
+        <Skelton count={1} height={61} />        
+    ) : (
+        <Link to={`/p/${username}`} className="grid -grid-cols-4 gap-4 mb-4 items-center">
+            <p>{username}</p>
+        </Link>
+    );
+    
+export default User;
