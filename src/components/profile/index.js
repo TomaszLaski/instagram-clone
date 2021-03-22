223dcbe6f411d8ce5e3732f0d95c6213ba72caac
TomaszLaski <tomasz.laski0001@gmail.com>
2021-03-22 17:32:23 +0100

    apply useReducer for stateful Profile page

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,8 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import Header from './header';
 import Photos from './photos';
+import { getUserByUsername } from '../../services/firebase';
+
+const reducer = (state, newState) => ({ ...state, ...newState });
+const initialState = {
+    profile: {},
+    photosCollection: [],
+    followerCount: 0
+};
 
 export default function Profile({ username }) {
+    const [{ profile, photosCollection, followerCount }, dispatch] = useReducer(
+        reducer,
+        initialState
+    );
+    
+    useEffect(() => {
+        async function getProfileInfoAndPhotos() {
+            const [{ ...user }] = await getUserByUsername(username);
+            console.log(user);
+        }
+        getProfileInfoAndPhotos();
+    }, [username]);
+    
     return (
         <>
             <Header />
