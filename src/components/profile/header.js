c00cf42f7574b5b7ffa7018a7ec800fc86c14962
TomaszLaski <tomasz.laski0001@gmail.com>
2021-03-22 19:37:01 +0100

    is User following active profile logic

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import useUser from '../../hooks/use-user';
-import { toggleFollow } from '../../services/firebase';
+import { isUserFollowingProfile, toggleFollow } from '../../services/firebase';
 
 export default function Header({ 
     photosCount,
-    followerCount,
+    followerCount: followers,
     setFollowerCount,
     username,
     profile: { docId: profileDocId, userId: profileUserId, fullName, following = [] }
@@ -17,10 +17,21 @@ export default function Header({
     const handleToggleFollow = async () => {
         setIsFollowingProfile((isFollowingProfile) => !isFollowingProfile);
         setFollowerCount({ 
-            followerCount: isFollowingProfile ? followerCount - 1 : followerCount + 1 
+            followerCount: isFollowingProfile ? followers - 1 : followers + 1 
         });
         await toggleFollow(isFollowingProfile, user.docId, profileDocId, profileUserId, user.userId);
-    }
+    };
+    
+    useEffect(() => {
+        const isLoggedInUserFollowingProfile = async () => {
+            const isFollowing = await isUserFollowingProfile(user.username, profileUserId);
+            setIsFollowingProfile(isFollowing);
+        };
+        
+        if (user.username && profileUserId) {
+            isLoggedInUserFollowingProfile();   
+        }
+    }, [user.username, profileUserId]);
     
     return (
         <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
@@ -45,7 +56,7 @@ export default function Header({
                     )}
                 </div>
                 <div className="container flex mt-4">
-                    {followerCount === undefined || following === undefined ? (
+                    {followers === undefined || following === undefined ? (
                         <Skeleton count={1} width={677} height={24} />
                     ) : (
                         <>
@@ -53,8 +64,8 @@ export default function Header({
                                 <span className="font-bold">{photosCount}</span> photos
                             </p>
                             <p className="mr-10">
-                                <span className="font-bold">{followerCount}</span> {' '}
-                                {followerCount === 1 ? 'follower' : 'followers'}
+                                <span className="font-bold">{followers}</span> {' '}
+                                {followers === 1 ? 'follower' : 'followers'}
                             </p>
                             <p className="mr-10">
                                 <span className="font-bold">{following.length}</span> following
